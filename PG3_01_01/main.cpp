d40335a1029a240fec4819d346bd872de2261dc9
oosakiharuki <k023g0035@g.neec.ac.jp>
2024-10-01 12:40:24 +0900

    01-01

---
+++ PG3_01_01/main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <iostream>
+#include <Windows.h>
+
+int main() {
+
+	printf("ああああ");
+
+	//SetConsoleOutputCP(65001);
+	//char str[] = "おおおお";
+	//printf("%s", str);
+
+	return 0;
+}
